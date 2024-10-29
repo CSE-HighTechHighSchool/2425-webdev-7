cb7cdb7f704e048c464a87126fc3c8e142ccbfb4
Harry Fan <118628756+realharryhero@users.noreply.github.com>
2024-10-29 12:29:47 -0400

    JS Scroll implemented

--- js/study-resources.js
+++ js/study-resources.js
@@ -1,14 +1,13 @@
 $(function() {
     $('#container .scroll').mousewheel(function(evt, chg) {
         var scrollval = document.getElementsByClassName("scroll")[0].scrollWidth;
-        alert(this.scrollY);
-        if(this.scrollLeft < scrollval) {
+        if(this.scrollLeft < scrollval - 913) {
             this.scrollLeft -= (chg * 100); //need a value to speed up the change
             //if (window.scrollY === 0) {
                 evt.preventDefault();
             //}
           }
-          if(this.scrollLeft >= scrollval) {
+          if(this.scrollLeft >= scrollval - 913) {
             if(evt.originalEvent.wheelDelta /120 > 0) {
                 if (window.scrollY === 0) {
                 this.scrollLeft -= (chg * 100); //need a value to speed up the change
@@ -17,3 +16,4 @@ $(function() {
           }
     });
  });
+ 
